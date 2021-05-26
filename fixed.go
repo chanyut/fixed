446036e099d6c5eddc4a180d1e740bbd4c1db63b
Chanyut Leecharoen <chanyutx10@gmail.com>
2021-05-26 23:47:19 +0700

    + pao non big - fix sql-driver compose decompose decimal bug

--- fixed.go
+++ fixed.go
@@ -3,6 +3,7 @@ package fixed
 // release under the terms of file license.txt
 
 import (
+	"database/sql/driver"
 	"encoding/binary"
 	"errors"
 	"fmt"
@@ -448,4 +449,61 @@ func (f Fixed) MarshalJSON() ([]byte, error) {
 	}
 	buffer := make([]byte, 24)
 	return itoa(buffer, f.fp), nil
+}
+
+// Value implements the driver.Valuer interface for database serialization.
+func (f Fixed) Value() (driver.Value, error) {
+	fmt.Println(">>>>>>>>> value")
+	return f.String(), nil
+}
+
+// Scan implements the sql.Scanner interface for database deserialization.
+func (d *Fixed) Scan(value interface{}) error {
+	// first try to see if the data is stored in database as a Numeric datatype
+	switch v := value.(type) {
+
+	case float32:
+		*d = NewF(float64(v))
+		return nil
+
+	case float64:
+		// numeric in sqlite3 sends us float64
+		*d = NewF(v)
+		return nil
+
+	case int64:
+		// at least in sqlite3 when the value is 0 in db, the data is sent
+		// to us as an int64 instead of a float64 ...
+		*d = NewI(v, 0)
+		return nil
+
+	default:
+		// default is trying to interpret value stored as string
+		str, err := unquoteIfQuoted(v)
+		if err != nil {
+			return err
+		}
+		*d, err = NewSErr(str)
+		return err
+	}
+}
+
+func unquoteIfQuoted(value interface{}) (string, error) {
+	var bytes []byte
+
+	switch v := value.(type) {
+	case string:
+		bytes = []byte(v)
+	case []byte:
+		bytes = v
+	default:
+		return "", fmt.Errorf("could not convert value '%+v' to byte array of type '%T'",
+			value, value)
+	}
+
+	// If the amount is quoted, strip the quotes
+	if len(bytes) > 2 && bytes[0] == '"' && bytes[len(bytes)-1] == '"' {
+		bytes = bytes[1 : len(bytes)-1]
+	}
+	return string(bytes), nil
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/robaho/fixed
+
+go 1.16
+
+require github.com/shopspring/decimal v1.2.0
